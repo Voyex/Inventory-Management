6d6ebbdc762175a5f2ebf4d9ba768c9c2c6e9581
Mitchell Haley <mitchhaley5@gmail.com>
2022-03-02 15:45:38 -0600

    Added meta descrition

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 #Variable that is used to set the title of the page.
 $PageTitle = "Inv Manager - Home";
 
+#Variable that is used to set the meta description of the page.
+$PageDesc = "A Short Descrption";
+
 function customPageHeader()
 { ?>
   <!-- Place custom HTML for the head tag here -->
--- template/header.php
+++ template/header.php
@@ -8,7 +8,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <meta name="description" content="<?= isset($PageDesc) ? $PageDesc : "Description" ?>">
+
     <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="/css/global.css">
     <link rel="stylesheet" href="/css/header.css">
