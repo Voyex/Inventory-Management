6c7360a660a2c2f693b6da5f61a88f7a42fb9c2b
Mitchell Haley <mitchhaley5@gmail.com>
2022-03-07 12:59:14 -0600

    migrated HTML to PHP

--- index.php
+++ index.php
@@ -14,9 +14,73 @@ function customPageHeader()
 include_once('template/header.php');
 ?>
 
-<div class = "homepage">
-  <div class="filter">Filter</div>
-  <div class="content">Content box</div>
+<div class="H-filter">Filter</div>
+      <div class="H-content">
+        <div class="shirts">
+          <div class="image">
+            <img src="shirtpic.jpg" alt="Shirt Picture" />
+          </div>
+          <div class="description">
+            <div class="top">
+              <h1>T-Shirts</h1>
+            </div>
+            <div class="middle">
+              <h2>Come in many sizes and colors</h2>
+            </div>
+          </div>
+          <div class="shirt-btn">
+            <button class="button-shirt">Buy Now!</button>
+          </div>
+        </div>
+        <div class="sweatshirts">
+          <div class="sweatshirt-image">
+            <img src="sweatshirt1.jpg" alt="Sweatshirt Picture" />
+          </div>
+          <div class="description">
+            <div class="topSweat">
+              <h1>Sweatshirts</h1>
+            </div>
+            <div class="middle">
+              <h2>Come in many sizes and colors</h2>
+            </div>
+          </div>
+          <div class="shirt-btn">
+            <button class="button-shirt">Buy Now!</button>
+          </div>
+        </div>
+        <div class="pants">
+          <div class="sweatshirt-image">
+            <img src="pants.jpg" alt="Sweatshirt Picture" />
+          </div>
+          <div class="description">
+            <div class="topSweat">
+              <h1>Pants</h1>
+            </div>
+            <div class="middle">
+              <h2>Come in many sizes and colors</h2>
+            </div>
+          </div>
+          <div class="shirt-btn">
+            <button class="button-shirt">Buy Now!</button>
+          </div>
+        </div>
+        <div class="socks">
+          <div class="sweatshirt-image">
+            <img src="socks.jpg" alt="Sweatshirt Picture" />
+          </div>
+          <div class="description">
+            <div class="topSweat">
+              <h1>Socks</h1>
+            </div>
+            <div class="middle">
+              <h2>Come in many sizes and colors</h2>
+            </div>
+          </div>
+          <div class="shirt-btn">
+            <button class="button-shirt">Buy Now!</button>
+          </div>
+        </div>
+</div>
 </div>
 
 <?php include_once('template/footer.php');
