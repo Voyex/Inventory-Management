41ec536beb0f39aa1fa4fffc341c43a8cb3017cd
Brendan Pelto <peltobr1555@uwec.edu>
2022-03-10 18:12:58 -0600

    admin page

--- admin.php
+++ admin.php
@@ -11,6 +11,100 @@ function customPageHeader()
 include_once('template/header.php');
 ?>
 
+<div class="H-filter">
+  <label for="dropdown" id="dropdown-label">Filter &and;</label>
+  <input type="checkbox" name="dropdown" class="dropdown" id="dropdown" autocomplete="off" onclick="toggleFilterDropdown(this)" />
+  <ul class="filter-content">
+    <li>Lorem ipsum</li>
+  </ul>
+</div>
+
+<form action="" class="search">
+  <input type="text" id="search-bar" placeholder="Search..." /><input type="submit" value="Search">
+</form>
+
+<div class="H-content">
+  <div class="item">
+    <a class="main-image" href="item.php">
+      <img src="shirtpic.jpg" alt="Shirt Picture" class="active-image" />
+    </a>
+    <div class="item-content">
+      <div class="top">
+        <a href="item.php" class="item-name">T-Shirts</a>
+      </div>
+      <div class="containQty">
+        <p>Color =</p>
+        <input class="colorText" type="text">
+        <p class="padd">Quantity= </p>
+        <input class="quantityText" type="text">
+      </div>
+      <p class="description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda voluptatibus voluptatem nesciunt praesentium ullam eos architecto odio perferendis cupiditate numquam amet magni, reiciendis commodi, voluptas eum hic eaque ex neque?
+      </p>
+      <!-- <button class="button-shirt">Buy Now!</button> -->
+    </div>
+  </div>
+  <div class="item">
+    <a class="main-image" href="item.php">
+      <img src="sweatshirt.webp" alt="Shirt Picture" class="active-image" />
+    </a>
+    <div class="item-content">
+      <div class="top">
+        <a href="item.php" class="item-name">Sweatshirts</a>
+      </div>
+      <div class="containQty">
+        <p>Color =</p>
+        <input class="colorText" type="text">
+        <p class="padd">Quantity= </p>
+        <input class="quantityText" type="text">
+      </div>
+      <p class="description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda voluptatibus voluptatem nesciunt praesentium ullam eos architecto odio perferendis cupiditate numquam amet magni, reiciendis commodi, voluptas eum hic eaque ex neque?
+      </p>
+      <!-- <button class="button-shirt">Buy Now!</button> -->
+    </div>
+  </div>
+  <div class="item">
+    <a class="main-image" href="item.php">
+      <img src="pants.jpg" alt="Shirt Picture" class="active-image" />
+    </a>
+    <div class="item-content">
+      <div class="top">
+        <a href="item.php" class="item-name">Pants</a>
+      </div>
+      <div class="containQty">
+        <p>Color =</p>
+        <input class="colorText" type="text">
+        <p class="padd">Quantity= </p>
+        <input class="quantityText" type="text">
+      </div>
+      <p class="description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda voluptatibus voluptatem nesciunt praesentium ullam eos architecto odio perferendis cupiditate numquam amet magni, reiciendis commodi, voluptas eum hic eaque ex neque?
+      </p>
+      <!-- <button class="button-shirt">Buy Now!</button> -->
+    </div>
+  </div>
+  <div class="item">
+    <a class="main-image" href="item.php">
+      <img src="socks.jpg" alt="Shirt Picture" class="active-image" />
+    </a>
+    <div class="item-content">
+      <div class="top">
+        <a href="item.php" class="item-name">Socks</a>
+      </div>
+      <div class="containQty">
+        <p>Color =</p>
+        <input class="colorText" type="text">
+        <p class="padd">Quantity= </p>
+        <input class="quantityText" type="text">
+      </div>
+      <p class="description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda voluptatibus voluptatem nesciunt praesentium ullam eos architecto odio perferendis cupiditate numquam amet magni, reiciendis commodi, voluptas eum hic eaque ex neque?
+      </p>
+      <!-- <button class="button-shirt">Buy Now!</button> -->
+    </div>
+  </div>
+</div>
 <!-- HTML goes here 
 Note: Don't add body or head tags as they are in the header 
 and are closed in the footer. Pretend they already exist.-->
