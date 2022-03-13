a60632dfc2949a36c2e2745f4951270854aedb03
Mitchell Haley <mitchhaley5@gmail.com>
2022-03-13 01:15:57 -0600

    started admin page

--- add-item.php
+++ add-item.php
@@ -1,107 +1,135 @@
 <?php
 #Variable that is used to set the title of the page.
-$PageTitle = "Inv Manager - Template";
+$PageTitle = "Inv Manager - Add Item";
 
 function customPageHeader()
 { ?>
     <!-- Place additional HTML for the head tag here -->
-    <link rel="stylesheet" href="css/admin.css" />
+    <link rel="stylesheet" href="css/index.css" />
 
 <?php }
 include_once('template/header.php');
 ?>
 
-<div class="H-filter">Filter</div>
-<div class="H-content">
-    <div class="shirts">
-        <!--<div class="image">
-            <img src="shirtpic.jpg" alt="Shirt Picture" />
-        </div>-->
-        <div class="description">
-            <div class="topSweat">
-                <h2>Shirts</h1>
-            </div>
-            <div class="middle">
-                <h2>What color quantity do you want to change?</h2>
-                <p>Red, Blue, Green, Black, White</p>
-                <input type="text" class="sweat-qty" placeholder="Item color">
+<div class="H-filter">
+  <label for="dropdown" id="dropdown-label">Filter &and;</label>
+  <input type="checkbox" name="dropdown" class="dropdown" id="dropdown" autocomplete="off" onclick="toggleFilterDropdown(this)" />
+  <ul class="filter-content">
+    <li>Lorem ipsum</li>
+  </ul>
+</div>
 
-                <h2>New Quantity?</h2>
-                <input type="text" class="sweat-qty" placeholder="Quantity">
-            </div>
-        </div>
-        <div class="shirt-btn">
-            <button class="button-shirt">Confirm Change</button>
-            <h1>Quantity:</h1>
-        </div>
+<form action="" class="search">
+  <input type="text" id="search-bar" placeholder="Search..." /><input type="submit" value="Search">
+</form>
+
+<div class="H-content">
+  <div class="item">
+    <a class="main-image" href="item.php">
+      <img src="shirtpic.jpg" alt="Shirt Picture" class="active-image" />
+    </a>
+    <div class="item-content">
+      <div class="top">
+        <a href="item.php" class="item-name">T-Shirts</a>
+      </div>
+      <div class="containQty">
+        <p>Color =</p>
+        <input class="colorText" type="text">
+        <p class="padd">Quantity= </p>
+        <input class="quantityText" type="text">
+        <button class="confirm">Confirm</button>
+      </div>
+      <div class="containNewQty">
+        <p class="description">
+          New Quantity =
+        </p>
+        <input type="0" class="output" disabled>
+      </div>
+      <!-- <button class="button-shirt">Buy Now!</button> -->
     </div>
-    <div class="sweatshirts">
-        <!--<div class="sweatshirt-image">
-            <img src="sweatshirt1.jpg" alt="Sweatshirt Picture" />
-        </div>-->
-        <div class="description">
-            <div class="topSweat">
-                <h2>Sweatshirts</h1>
-            </div>
-            <div class="middle">
-                <h2>What color quantity do you want to change?</h2>
-                <p>Red, Blue, Green, Black, White</p>
-                <input type="text" class="sweat-qty" placeholder="Item color">
-                <h2>New Quantity?</h2>
-                <input type="text" class="sweat-qty" placeholder="Quantity">
-            </div>
-        </div>
-        <div class="shirt-btn">
-            <button class="button-shirt">Confirm Change</button>
-            <h1>Quantity:</h1>
-        </div>
+  </div>
+  <div class="item">
+    <a class="main-image" href="item.php">
+      <img src="sweatshirt.webp" alt="Shirt Picture" class="active-image" />
+    </a>
+    <div class="item-content">
+      <div class="top">
+        <a href="item.php" class="item-name">Sweatshirts</a>
+      </div>
+      <div class="containQty">
+        <p>Color =</p>
+        <input class="colorText" type="text">
+        <p class="padd">Quantity= </p>
+        <input class="quantityText" type="text">
+        <button class="confirm">Confirm</button>
+
+      </div>
+      <div class="containNewQty">
+        <p class="description">
+          New Quantity =
+        </p>
+        <input type="0" class="output" disabled>
+      </div>
+      <!-- <button class="button-shirt">Buy Now!</button> -->
     </div>
-    <div class="pants">
-        <!--<div class="sweatshirt-image">
-            <img src="pants.jpg" alt="Sweatshirt Picture" />
-        </div>-->
-        <div class="description">
-            <div class="topSweat">
-                <h2>Pants</h1>
-            </div>
-            <div class="middle">
-                <h2>What color quantity do you want to change?</h2>
-                <p>Red, Blue, Green, Black, White</p>
-                <input type="text" class="sweat-qty" placeholder="Item color">
-                <h2>New Quantity?</h2>
-                <input type="text" class="sweat-qty" placeholder="Quantity">
-            </div>
-        </div>
-        <div class="shirt-btn">
-            <button class="button-shirt">Confirm Change</button>
-            <h1>Quantity:</h1>
-        </div>
+  </div>
+  <div class="item">
+    <a class="main-image" href="item.php">
+      <img src="pants.jpg" alt="Shirt Picture" class="active-image" />
+    </a>
+    <div class="item-content">
+      <div class="top">
+        <a href="item.php" class="item-name">Pants</a>
+      </div>
+      <div class="containQty">
+        <p>Color =</p>
+        <input class="colorText" type="text">
+        <p class="padd">Quantity= </p>
+        <input class="quantityText" type="text">
+        <button class="confirm">Confirm</button>
+
+      </div>
+      <div class="containNewQty">
+        <p class="description">
+          New Quantity =
+        </p>
+        <input type="0" class="output" disabled>
+      </div>
+      <!-- <button class="button-shirt">Buy Now!</button> -->
     </div>
-    <div class="socks">
-        <!--<div class="sweatshirt-image">
-            <img src="socks.jpg" alt="Sweatshirt Picture" />
-        </div>-->
-        <div class="description">
-            <div class="topSweat">
-                <h2>Socks</h1>
-            </div>
-            <div class="middle">
-                <h2>What color quantity do you want to change?</h2>
-                <p>Red, Blue, Green, Black, White</p>
-                <input type="text" class="sweat-qty" placeholder="Item color">
-                <h2>New Quantity?</h2>
-                <input type="text" class="sweat-qty" placeholder="Quantity">
-            </div>
-        </div>
-        <div class="shirt-btn">
-            <button class="button-shirt">Confirm Change</button>
-            <h1>Quantity:</h1>
-        </div>
+  </div>
+  <div class="item">
+    <a class="main-image" href="item.php">
+      <img src="socks.jpg" alt="Shirt Picture" class="active-image" />
+    </a>
+    <div class="item-content">
+      <div class="top">
+        <a href="item.php" class="item-name">Socks</a>
+      </div>
+      <div class="containQty">
+        <p>Color =</p>
+        <input class="colorText" type="text">
+        <p class="padd">Quantity= </p>
+        <input class="quantityText" type="text">
+        <button class="confirm">Confirm</button>
+
+      </div>
+      <div class="containNewQty">
+        <p class="description">
+          New Quantity =
+        </p>
+        <input type="text" class="output" placeholder="0" disabled>
+      </div>
+      <!-- <button class="button-shirt">Buy Now!</button> -->
     </div>
+  </div>
 </div>
 <!-- HTML goes here 
 Note: Don't add body or head tags as they are in the header 
 and are closed in the footer. Pretend they already exist.-->
+
+<script src="scripts/script.js"></script>
+
 
 <?php include_once('template/footer.php');
 // Dont place any code here
--- admin.php
+++ admin.php
@@ -1,129 +1,56 @@
 <?php
 #Variable that is used to set the title of the page.
-$PageTitle = "Inv Manager - Template";
+$PageTitle = "Inv Manager - Admin Tools";
 
 function customPageHeader()
 { ?>
   <!-- Place additional HTML for the head tag here -->
-  <link rel="stylesheet" href="css/index.css" />
+  <link rel="stylesheet" href="css/admin.css" />
 
 <?php }
 include_once('template/header.php');
 ?>
 
-<div class="H-filter">
-  <label for="dropdown" id="dropdown-label">Filter &and;</label>
-  <input type="checkbox" name="dropdown" class="dropdown" id="dropdown" autocomplete="off" onclick="toggleFilterDropdown(this)" />
-  <ul class="filter-content">
-    <li>Lorem ipsum</li>
-  </ul>
+<div class="table-container">
+  <form action="">
+    <table class="item-table">
+      <tr>
+        <th>SKU</th>
+        <th>Name</th>
+        <th>MSRP ($)</th>
+        <th>Purchase Price ($)</th>
+        <th>In Stock Qty.</th>
+        <th>Incoming Qty.</th>
+        <th>Outgoing Qty.</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+      <tr>
+        <td class="sku">ABC123</td>
+        <td class="title">T-Shirt</td>
+        <td class="msrp">25.00</td>
+        <td class="purchase-price">18.00</td>
+        <td class="in-stock" contenteditable="true">7</td>
+        <td class="incoming">41</td>
+        <td class="outgoing" contenteditable="true">2</td>
+        <td><button>Edit</button></td>
+        <td><button>Delete</button></td>
+      </tr>
+      <tr>
+        <td>A3B2C1</td>
+        <td>Jeans</td>
+        <td>35.00</td>
+        <td>28.00</td>
+        <td>3</td>
+        <td>19</td>
+        <td>0</td>
+        <td><button>Edit</button></td>
+        <td><button>Delete</button></td>
+      </tr>
+    </table>
+  </form>
 </div>
 
-<form action="" class="search">
-  <input type="text" id="search-bar" placeholder="Search..." /><input type="submit" value="Search">
-</form>
-
-<div class="H-content">
-  <div class="item">
-    <a class="main-image" href="item.php">
-      <img src="shirtpic.jpg" alt="Shirt Picture" class="active-image" />
-    </a>
-    <div class="item-content">
-      <div class="top">
-        <a href="item.php" class="item-name">T-Shirts</a>
-      </div>
-      <div class="containQty">
-        <p>Color =</p>
-        <input class="colorText" type="text">
-        <p class="padd">Quantity= </p>
-        <input class="quantityText" type="text">
-        <button class="confirm">Confirm</button>
-      </div>
-      <div class="containNewQty">
-        <p class="description">
-          New Quantity =
-        </p>
-        <input type="0" class="output" disabled>
-      </div>
-      <!-- <button class="button-shirt">Buy Now!</button> -->
-    </div>
-  </div>
-  <div class="item">
-    <a class="main-image" href="item.php">
-      <img src="sweatshirt.webp" alt="Shirt Picture" class="active-image" />
-    </a>
-    <div class="item-content">
-      <div class="top">
-        <a href="item.php" class="item-name">Sweatshirts</a>
-      </div>
-      <div class="containQty">
-        <p>Color =</p>
-        <input class="colorText" type="text">
-        <p class="padd">Quantity= </p>
-        <input class="quantityText" type="text">
-        <button class="confirm">Confirm</button>
-
-      </div>
-      <div class="containNewQty">
-        <p class="description">
-          New Quantity =
-        </p>
-        <input type="0" class="output" disabled>
-      </div>
-      <!-- <button class="button-shirt">Buy Now!</button> -->
-    </div>
-  </div>
-  <div class="item">
-    <a class="main-image" href="item.php">
-      <img src="pants.jpg" alt="Shirt Picture" class="active-image" />
-    </a>
-    <div class="item-content">
-      <div class="top">
-        <a href="item.php" class="item-name">Pants</a>
-      </div>
-      <div class="containQty">
-        <p>Color =</p>
-        <input class="colorText" type="text">
-        <p class="padd">Quantity= </p>
-        <input class="quantityText" type="text">
-        <button class="confirm">Confirm</button>
-
-      </div>
-      <div class="containNewQty">
-        <p class="description">
-          New Quantity =
-        </p>
-        <input type="0" class="output" disabled>
-      </div>
-      <!-- <button class="button-shirt">Buy Now!</button> -->
-    </div>
-  </div>
-  <div class="item">
-    <a class="main-image" href="item.php">
-      <img src="socks.jpg" alt="Shirt Picture" class="active-image" />
-    </a>
-    <div class="item-content">
-      <div class="top">
-        <a href="item.php" class="item-name">Socks</a>
-      </div>
-      <div class="containQty">
-        <p>Color =</p>
-        <input class="colorText" type="text">
-        <p class="padd">Quantity= </p>
-        <input class="quantityText" type="text">
-        <button class="confirm">Confirm</button>
-
-      </div>
-      <div class="containNewQty">
-        <p class="description">
-          New Quantity =
-        </p>
-        <input type="text" class="output" placeholder="0" disabled>
-      </div>
-      <!-- <button class="button-shirt">Buy Now!</button> -->
-    </div>
-  </div>
-</div>
 <!-- HTML goes here 
 Note: Don't add body or head tags as they are in the header 
 and are closed in the footer. Pretend they already exist.-->
