979a605b2e420ab8ddbd107e04c4811403b0f1e1
Mitchell Haley <mitchhaley5@gmail.com>
2022-04-11 13:05:58 -0500

    Added signout button

--- template/header.php
+++ template/header.php
@@ -32,6 +32,7 @@ session_start();
                 <li><a href="admin.php" class="nav-link">Admin</a></li>
                 <li><a href="cart.php" class="nav-link">Cart</a></li>
                 <li><a href="login.php" class="nav-link">Login</a></li>
+                <li><a href="includes/signout.inc.php"></a></li>
             </ul>
         </nav>
     </header>
